0871d7874993f108f055058b5d94f7e5ffb827f9
Ali Arslan Ansari <aliarslan1620@gmail.com>
2024-07-02 17:59:07 +0530

    Added Inter font as global font-family

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,15 @@
 /** @type {import('tailwindcss').Config} */
+
+import defaultTheme from "tailwindcss/defaultTheme";
+
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        inter: ['"Inter"', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 };
-

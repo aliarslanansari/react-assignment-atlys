e64bcc6dfd1bc9b1e29779aabddcaa01f43b5bb1
Ali Arslan Ansari <aliarslan1620@gmail.com>
2024-07-05 05:05:35 +0530

    add hash based router to make it work on vercel deployment

--- src/main.tsx
+++ src/main.tsx
@@ -4,16 +4,16 @@ import { persistor, store } from "@/redux/store";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter>
+        <HashRouter>
           <App />
-        </BrowserRouter>
+        </HashRouter>
       </PersistGate>
     </Provider>
   </React.StrictMode>,
